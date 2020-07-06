90ed878fb42b985c56e2afd1c05c9d39181e1138
danielebuso <daniele@kodesire.com>
2020-07-06 14:08:28 +0200

    Fixed function

--- src/Shortener.php
+++ src/Shortener.php
@@ -21,13 +21,13 @@ class Shortener
      */
     public static function shorten(string $url, array $opt = [])
     {
-        $link_length = $opt['length'] || config('shortener.link_length') || 8;
+        $link_length = $opt['length'] ?? config('shortener.link_length') ?? 8;
 
         return ShortLink::create([
             'id' => Str::random($link_length),
             'url' => $url,
-            'expire_at' => $opt['expire_at'] || null,
-            'not_before' => $opt['not_before'] || null,
+            'expire_at' => $opt['expire_at'] ?? null,
+            'not_before' => $opt['not_before'] ?? null,
         ]);
     }
 
